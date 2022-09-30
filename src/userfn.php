20031f656770b44f43142bd9fcc168d8c165449d
Melissa <0himel.cv@gmail.com>
2022-09-30 17:38:14 +0200

    api

--- src/userfn.php
+++ src/userfn.php
@@ -211,7 +211,7 @@ function Api_Action($app) {
             $response = $response->withJson(ExecuteRows("SELECT votos_jugador from jugador WHERE ID_JUGADOR= '" . AdjustSql($ID_EQUIPO) . "'"));
             //$p= json_decode($response, true);
             echo 'hola';
-            print_r(json_decode( stripslashes($response) , true ));
+            print_r(json_decode( preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $response), true ));
        }    
         return $response;
     });
